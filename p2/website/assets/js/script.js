e23366b0dd00976a5e0d1ab12b9845dc18a24219
Manasvi <manasvi@cca.edu>
2018-03-15 16:03:37 -0700

    taxo

---
+++ p2/website/assets/js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+
+
+
+$('.story5').click(function(){
+    $('body').removeClass();
+    $('body').addClass('active5');
+});
+
+// $('.story5').click(function(){
+//     $('body').addClass('active6');
+// });
+
+$('.story6').click(function(){
+	$('body').removeClass();
+    $('body').addClass('active6');
+});
+
+$('.story7').click(function(){
+	$('body').removeClass();
+    $('body').addClass('active7');
+});
+
+$('.story8').click(function(){
+	$('body').removeClass();
+    $('body').addClass('active8');
+
+});
+
+$('.story9').click(function(){
+	$('body').removeClass();
+    $('body').addClass('active9');
+
+});
+
+$('.story10').click(function(){
+	$('body').removeClass();
+    $('body').addClass('active10');
+
+});
+
+$('.story11').click(function(){
+	$('body').removeClass();
+    $('body').addClass('active11');
+
+});
+
+$('.item3').click(function() {
+    $('.item78').fadeIn();
+});
+
+
+
+
+
+// $('.item28').hover(function() {
+//     $('.enlarge').fadeIn();
+// });
+
+
+
+});
